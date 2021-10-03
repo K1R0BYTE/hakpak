ecc0901d23a910b79548ad447a24507c62cf6e83
Kirill Murzin <kirill.murzin@gmail.com>
2021-10-03 15:13:58 +0600

    CRLF to LF

--- install/wordlists.sh
+++ install/wordlists.sh
@@ -10,9 +10,10 @@ colorecho() {
 colorecho 'Starting Installing Wordlists'
 
 # SecLists
-wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O /tmp/SecLists.zip \
-  && unzip SecList.zip -d /opt/wordlists/seclists \
-  && rm -f /tmp/SecLists.zip
+wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O /tmp/SecLists.zip
+unzip SecList.zip -d /opt/wordlists/seclists
+rm -f /tmp/SecLists.zip
+
 # Fuzz
 wget -c https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/fuzz.txt -O /opt/wordlists/fuzz.txt
 
